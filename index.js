bd870c55dbfde937dc2a92872079ab62800dcc21
Bhavika Mevada <bmevada>
2022-09-25 19:51:23 +1000

    add content required - questions

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 // TODO: Include packages needed for this application
 
+const inquirer = require('inquirer');
+const fs = require ('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+console.log("Welcome to the README document generator")
+console.log("A professional README document generator has been created to allow the user to quickly and easily generate a README file by using a command-line application to generate one. This allows the project creator to devote more time working on the project.")
+
 // TODO: Create an array of questions for user input
 const questions = [];
+    // Title of the project
+    // Description of the project
+    //  Installation
+    //  Usage
+    //  Contributing
+    //  Tests
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
