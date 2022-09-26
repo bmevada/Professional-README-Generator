455c07550bc6ddc89aa0e0c1ca01a79cb666fcb2
Bhavika Mevada <bmevada>
2022-09-26 19:34:40 +1000

    badges for licences

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,22 +2,22 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (license === 'Apache 2.0'){
-    return '![License: Apache]()'
+    return '![License: Apache](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
   }
   if (license === 'MIT License'){
-    return '![License: MIT License]()'
+    return '![License: MIT License](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
   }
   if (license === 'Mozilla Public License 2.0'){
-    return '![License: Mozilla Public License 2.0]()'
+    return '![License: Mozilla Public License 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)'
   }
   if (license === 'Boost Software License 1.0'){
-    return '![License: Boost Software License 1.0]()'
+    return '![License: Boost Software License 1.0](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)'
   }
   if (license === 'The Unilicense'){
-    return '![License: The Unilicense]()'
+    return '![License: The Unilicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)'
   }
   if (license === 'GNU AGPLv3'){
-    return '![License: GNU AGPLv3]()'
+    return '![License: GNU AGPLv3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)'
   }
 }
 
@@ -25,7 +25,7 @@ function renderLicenseBadge(license) {
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
   if (license === 'Apache 2.0'){
-    return '![License: Apache](https://choosealicense.com/licenses/apache-2.0/)'
+    return '![License: Apache](https://www.apache.org/licenses/LICENSE-2.0)'
   }
   if (license === 'MIT License'){
     return '![License: MIT License](https://choosealicense.com/licenses/mit/)'
