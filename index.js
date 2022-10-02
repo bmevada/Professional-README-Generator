2d38f90d240c8cff04e07eacb55d6dafd6af0f75
Bhavika Mevada <bmevada>
2022-10-02 21:35:07 +1100

    error identified

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ function writeToFile(fileName, data) {
 function init() {
    inquirer.prompt(questions).then((inquirerResponses) => {
     console.log('Generating README...');
-    writeToFile('README.md', generateMarkdown({ ...inquirerResponses}));
+    writeToFile('README.md', generateMarkdown({ ...inquirerResponses }));
    })
 }
 
