0b9cbc6bf9726d184599c4214417a4de235466b2
Bhavika Mevada <bmevada>
2022-10-02 20:52:45 +1100

    error fixed in index license

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ const questions = [
         type: 'list',
         name: 'license',
         message: 'What licences are required for this application?',
-        choices: ['Apache-License-2.0', 'MIT-License', 'Mozilla-Public-License-2.0', 'Boost-Software-License-1.0', 'The-Unilicense'. 'GNU-AGPLv3', 'None']
+        choices: ['Apache', 'MIT', 'Mozilla', 'Boost', 'Unilicense', 'GNU-AGPLv3', 'None']
     },
     {   //Contributing
         type: 'input',
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -19,7 +19,7 @@ function renderLicenseBadge(license) {
   // if (license === 'GNU AGPLv3'){
   //   return '![License: GNU AGPLv3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)'
   // }
-  if (license !== 'None'){
+  if (license !== 'None') {
     return `![Github]('https://img.shields.io/badge/License-${license}-blue.svg)`;
   }
 }
@@ -45,7 +45,7 @@ function renderLicenseLink(license) {
   // if (license === 'GNU AGPLv3'){
   //   return '![License: GNU AGPLv3](https://choosealicense.com/licenses/agpl-3.0/)'
   // }
-  if (license !== 'None'){
+  if (license !== 'None') {
     return `[License](#license)`;
   }
   return '';
@@ -55,18 +55,18 @@ function renderLicenseLink(license) {
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
   // switch (license) {
-    if (license !== 'None') {
-      return `## License
+  if (license !== 'None') {
+    return `## License
       license used is ${license}.`
-    }
-    else {
-      return '';
-    }
+  }
+  else {
+    return '';
+  }
 
-    // case 'GNU GPLv3 License':
-    //   return `This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-    //   This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-    //   You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.`;
+  // case 'GNU GPLv3 License':
+  //   return `This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+  //   This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+  //   You should have received a copy of the GNU General Public License along with this program. If not, see <https://www.gnu.org/licenses/>.`;
   // }
 };
 
